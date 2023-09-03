e94d6f4b0dad104fcf84bd0d95f0bbbe30ec4ad1
Revenko Vladislav <revad8@gmail.com>
2023-09-03 21:58:09 +0300

    add AuthorRepository and GenreRepository

---
+++ Repositories/AuthorRepository.cs
@@ -0,0 +1,18 @@
+﻿using Module25.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module25.Repositories
+{
+    public class AuthorRepository
+    {
+        public static void AddAuthor(AppContext db, Author author)
+        {
+            db.Authors.Add(author);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Repositories/GenreRepository.cs
@@ -0,0 +1,18 @@
+﻿using Module25.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module25.Repositories
+{
+    public class GenreRepository
+    {
+        public static void AddGenre(AppContext db, Genre genre)
+        {
+            db.Genres.Add(genre);
+            db.SaveChanges();
+        }
+    }
+}
